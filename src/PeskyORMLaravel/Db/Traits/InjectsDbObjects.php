542b074698fdf205f51dd8eaa759ce20e1769b8d
Filippov Aleksandr <swayalex@gmail.com>
2017-09-20 18:22:15 +0300

    Added InjectsDbObjects and InjectsDbObjectsAndValidatesOwner traits

---
+++ src/PeskyORMLaravel/Db/Traits/InjectsDbObjects.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace PeskyORMLaravel\Db\Traits;
+
+use Illuminate\Routing\Route;
+use PeskyORM\ORM\RecordInterface;
+
+trait InjectsDbObjects {
+
+    protected function injectOnlyActiveNotSoftdeletedObjects() {
+        return true;
+    }
+
+    public function callAction($method, $parameters) {
+        $this->readDbObjectForInjection($parameters);
+        return parent::callAction($method, $parameters);
+    }
+
+    /**
+     * @param $parameters
+     * @throws \BadMethodCallException
+     * @throws \InvalidArgumentException
+     * @throws \PDOException
+     * @throws \PeskyORM\Exception\OrmException
+     * @throws \UnexpectedValueException
+     * @throws \PeskyORM\Exception\InvalidDataException
+     */
+    protected function readDbObjectForInjection($parameters) {
+        /** @var Route $route */
+        $route = \Request::route();
+        $object = null;
+        foreach ($parameters as $key => $value) {
+            if ($value instanceof RecordInterface) {
+                // get only last object in params
+                $object = $value;
+            }
+        }
+        if (!empty($object)) {
+            $id = $route->parameter('id', false);
+            if ($id === false && \Request::method() !== 'GET') {
+                 $id = \Request::get('id', false);
+            }
+            if (empty($id)) {
+                $this->sendRecordNotFoundResponse();
+            }
+            $conditions = [
+                $object::getTable()->getPkColumnName() => $id,
+            ];
+            $this->addConditionsForDbObjectInjection($route, $object, $conditions);
+            if ($this->injectOnlyActiveNotSoftdeletedObjects()) {
+                $this->addIsActiveAndIsDeletedConditionsForDbObjectInjection($route, $object, $conditions);
+            }
+            $this->addParentIdsConditionsForDbObjectInjection($route, $object, $conditions);
+            $object->fromDb($conditions);
+            if (!$object->existsInDb()) {
+                $this->sendRecordNotFoundResponse();
+            }
+        }
+    }
+
+    /**
+     * Abort with HTTP code 404
+     */
+    protected function sendRecordNotFoundResponse() {
+        abort(404, 'Record not found in DB');
+    }
+
+    /**
+     * @param Route $route
+     * @param RecordInterface $object
+     * @param array $conditions
+     */
+    protected function addConditionsForDbObjectInjection(Route $route, RecordInterface $object, array &$conditions) {
+
+    }
+
+    /**
+     * @param Route $route
+     * @param RecordInterface $object
+     * @param array $conditions
+     */
+    protected function addIsActiveAndIsDeletedConditionsForDbObjectInjection(Route $route, RecordInterface $object, array &$conditions) {
+        if ($object::getTable()->getTableStructure()->hasColumn('is_active')) {
+            $conditions['is_active'] = (bool)$route->parameter('is_active', true);
+        }
+        if ($object::getTable()->getTableStructure()->hasColumn('is_deleted')) {
+            $conditions['is_deleted'] = (bool)$route->parameter('is_deleted', false);
+        }
+    }
+
+    /**
+     * @param Route $route
+     * @param RecordInterface $object
+     * @param array $conditions
+     * @throws \BadMethodCallException
+     * @throws \InvalidArgumentException
+     * @throws \PeskyORM\Exception\OrmException
+     * @throws \UnexpectedValueException
+     */
+    protected function addParentIdsConditionsForDbObjectInjection(Route $route, RecordInterface $object, array &$conditions) {
+        foreach ($route->parameterNames() as $name) {
+            if ($object::hasColumn($name)) {
+                $conditions[$name] = $route->parameter($name);
+            }
+        }
+    }
+}
